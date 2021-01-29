29c00534ea9b863e9ee5fe0a1e77ffd388235c72
philoXof <jehanno.lucas@gmail.com>
2021-01-29 12:17:23 +0100

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
-//ej.base.enableRipple(true);
-//ej.diagrams.Diagram.Inject(ej.diagrams.UndoRedo);
-
 var diagram;
 
-//CREATION DES DIFFERENTS ELEMENTS AVEC TAILLE POSITION COULEURS
-var nodes = [
+var node = [
   {
     id: "node1",
-    style: { fill: "#FFB2B2", strokeColor: "#FFB2B2" },
-    shape: { type: "Flow", shape: "Terminator" },
-    annotations: [{ id: "label1", content: "Start" }]
+    width: 100,
+    height: 100,
+    offsetX: 250,
+    offsetY: 250,
+    annotations: [
+      {
+        id: "label",
+        content: "Rectangle",
+        offset: {
+          x: 0.5,
+          y: 0.5
+        },
+        style: {
+          color: "white"
+        }
+      }
+    ],
+
+    style: {
+      strokeColor: "#f0f",
+      fill: "#f0f"
+    },
+    constraints:
+      ej.diagrams.NodeConstraints.None | ej.diagrams.NodeConstraints.Shadow,
+    shadow: { angle: 40, opacity: 0.8, distance: 6 }
   },
   {
     id: "node2",
-    style: { fill: "#DCDCDC", strokeColor: "#DCDCDC" },
-    shape: { type: "Flow", shape: "Process" },
-    annotations: [{ id: "label1", content: "Process" }]
-  },
-  {
-    id: "node3",
-    style: { fill: "#DCDCDC", strokeColor: "#DCDCDC" },
-    annotations: [{ id: "label1", content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
+    width: 100,
+    height: 100,
+    offsetX: 350,
+    offsetY: 350,
+    annotations: [
+      {
+        id: "label2",
+        content: "Triangle",
+        offset: {
+          x: 0.5,
+          y: 0.5
+        },
+        style: {
+          color: "white"
+        }
+      }
+    ],
+
+    style: {
+      strokeColor: "#f0f",
+      fill: "#f0f"
+    },
+    constraints:
+      ej.diagrams.NodeConstraints.None | ej.diagrams.NodeConstraints.Shadow,
+    shadow: { angle: 40, opacity: 0.8, distance: 6 }
+    // ne peux pas interagir avec ce noeud !
   }
 ];
 
-//LIENS ENTRE LES ELEMENTS
-var connectors = [];
-
-var snapSettings = {
-  constraints:
-    ej.diagrams.SnapConstraints.SnapToLines |
-    ej.diagrams.SnapConstraints.ShowLines
-  //ej.diagrams.SnapConstraints.ShowLines pour afficher les lignes en fond du diagram
-};
-
-diagram = new ej.diagrams.Diagram({
-  width: "100%",
-  height: "100%", //600px
-  //border: "1px",
-  nodes: nodes,
-
-  //tool: DiagramTools.None,
-  connectors: connectors,
-  getNodeDefaults: getNodeDefaults,
-  getConnectorDefaults: function(obj) {
-    (obj.height = 10), (obj.style.fill = "#00ff00");
-    obj.targetDecorator.style.fill = "#ff0000"; //remplissage fleche
-    obj.targetDecorator.style.strokeColor = "#ff0000"; //contour fleche
+//diagram
+diagram = new ej.diagrams.Diagram(
+  {
+    width: "100%",
+    height: "100%",
+    constraints:
+      ej.diagrams.DiagramConstraints.Default |
+      ej.diagrams.DiagramConstraints.Tooltip,
+    nodes: node
   },
-  /*getNodeDefaults: function(node) {
-    node.height = 10;
-    node.width = 10;
-    node.style.fill = "#0f0";
-    node.style.strokeColor = "0f0";
-    return node;
-  },*/
-  snapSettings: snapSettings
-});
-diagram.appendTo("#diagram");
-
-function getNodeDefaults(obj) {
-  obj.annotations[0].style.color = "#111111";
-  return obj;
-}
-
-diagram.fitToPage({ mode: "Height" });
+  "#diagram"
+);
