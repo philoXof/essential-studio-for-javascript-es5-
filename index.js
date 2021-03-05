e5ae97c6ad05730a87cfbfcba8b083249b8a5e04
philoXof <jehanno.lucas@gmail.com>
2021-03-05 16:28:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-ej.diagrams.Diagram.Inject(ej.diagrams.LineRouting);
-var diagram;
-const sizeNode = 60;
-
 /////////////////////////////////////////////
 /*                  DATA                   */
 /////////////////////////////////////////////
@@ -128,272 +124,29 @@ var data = [
     shape: "Terminator"
   }
 ];
-
-/////////////////////////////////////////////
-/*                  NODE                   */
-/////////////////////////////////////////////
-/*
-var node = [
-  {
-    id: "node1",
-    offsetX: 0,
-    offsetY: 200,
-    shape: { type: "Flow", shape: "Terminator" },
-    annotations: [{ content: "Start" }]
-  },
-  {
-    id: "node2",
-    offsetX: 100,
-    offsetY: 200,
-    shape: { type: "Flow", shape: "Process" },
-    annotations: [{ content: "Process" }]
-  },
-  {
-    id: "node3",
-    offsetX: 200,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node4",
-    offsetX: 300,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node5",
-    offsetX: 400,
-    offsetY: 200,
-    annotations: [{ content: "1" }],
-    shape: { type: "Flow", shape: "Decision" }
-  },
-
-  {
-    id: "node6",
-    offsetX: 500,
-    offsetY: 200,
-    annotations: [{ content: "" }],
-    shape: { type: "Flow", shape: "Or" }
-  },
-  {
-    id: "node7",
-    offsetX: 600,
-    offsetY: 50,
-    annotations: [{ content: "" }],
-    shape: { type: "Flow", shape: "Decision" }
-  },
-  {
-    id: "node8",
-    offsetX: 700,
-    offsetY: 50,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node9",
-    offsetX: 600,
-    offsetY: 200,
-    annotations: [{ content: "" }],
-    shape: { type: "Flow", shape: "Or" }
-  },
-  {
-    id: "node10",
-    offsetX: 700,
-    offsetY: 150,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node11",
-    offsetX: 700,
-    offsetY: 250,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node12",
-    offsetX: 600,
-    offsetY: 350,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node13",
-    offsetX: 700,
-    offsetY: 350,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node14",
-    offsetX: 800,
-    offsetY: 200,
-    //style: { fill: "#0f0", strokeColor: "#0f0" }, // ENTIEREMENT VERT (intérieur/contour)
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node15",
-    offsetX: 900,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node16",
-    offsetX: 1000,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node17",
-    offsetX: 1100,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node18",
-    offsetX: 1200,
-    offsetY: 200,
-    annotations: [{ content: "" }],
-    shape: { type: "Flow", shape: "Decision" }
-  },
-  {
-    id: "node19",
-    offsetX: 1300,
-    offsetY: 200,
-    annotations: [{ content: "Process" }],
-    shape: { type: "Flow", shape: "Process" }
-  },
-  {
-    id: "node20",
-    offsetX: 1400,
-    offsetY: 200,
-    annotations: [
-      {
-        content: "End"
-      }
-    ],
-    shape: { type: "Flow", shape: "Terminator" }
-  }
-];
-*/
-
-/////////////////////////////////////////////
-/*                CONNECTOR                */
-/////////////////////////////////////////////
-
-/*
-var connector = [
-  { id: "connector1", sourceID: "node1", targetID: "node2" },
-  { id: "connector2", sourceID: "node2", targetID: "node3" },
-  { id: "connector3", sourceID: "node3", targetID: "node4" },
-  { id: "connector4", sourceID: "node4", targetID: "node5" },
-  { id: "connector5", sourceID: "node5", targetID: "node6" },
-  { id: "connector6", sourceID: "node6", targetID: "node7" },
-  { id: "connector7", sourceID: "node6", targetID: "node9" },
-  { id: "connector8", sourceID: "node6", targetID: "node12" },
-  { id: "connector9", sourceID: "node12", targetID: "node13" },
-  { id: "connector10", sourceID: "node9", targetID: "node10" },
-  { id: "connector11", sourceID: "node9", targetID: "node11" },
-  { id: "connector12", sourceID: "node10", targetID: "node14" },
-  { id: "connector13", sourceID: "node11", targetID: "node14" },
-  { id: "connector14", sourceID: "node7", targetID: "node8" },
-  { id: "connector15", sourceID: "node8", targetID: "node14" },
-  { id: "connector16", sourceID: "node13", targetID: "node14" },
-  { id: "connector17", sourceID: "node14", targetID: "node15" },
-  { id: "connector18", sourceID: "node15", targetID: "node16" },
-  { id: "connector19", sourceID: "node16", targetID: "node17" },
-  { id: "connector20", sourceID: "node17", targetID: "node18" },
-  { id: "connector21", sourceID: "node18", targetID: "node19" },
-  { id: "connector22", sourceID: "node19", targetID: "node20" },
-  { id: "connector23", sourceID: "node5", targetID: "node15" },
-  { id: "connector24", sourceID: "node5", targetID: "node19" },
-  { id: "connector25", sourceID: "node18", targetID: "node20" }
-];*/
-/*var connectorEX = [
-  //1
-  {
-    id: "connector1",
-    sourceID: "node1",
-    targetID: "node2"
-  },
-  //2
-  {
-    id: "connector2",
-    sourcePoint: { x: 100, y: 200 },
-    targetPoint: { x: 400, y: 400 }
-  }
-];*/
+data.concat(Array[("node21", ["node1", "node2"], "node21", "Process")]);
 
 /////////////////////////////////////////////
 /*                 DIAGRAM                 */
 /////////////////////////////////////////////
-/*
-diagram = new ej.diagrams.Diagram(
-  {
-    width: "100%",
-    height: "100%",
-
-    getNodeDefaults: function(node) {
-      node.height = sizeNode;
-      node.width = sizeNode;
-      node.style = {
-        strokeColor: "#FFB2B2",
-        fill: "#DCDCDC"
-      };
-      return node;
-    },
-
-    getConnectorDefaults: function(connector) {
-      connector.type = "Orthogonal";
-      connector.cornerRadius = 10;
-      connector.style = {
-        strokeColor: "#0f0",
-        fill: "#f0f",
-        strokeWidth: 2
-      };
-      connector.targetDecorator = {
-        style: {
-          fill: "#0f0",
-          strokeColor: "#0f0"
-        }
-      };
-      return connector;
-    },
-
-    constraints:
-      ej.diagrams.DiagramConstraints.Default |
-      ej.diagrams.DiagramConstraints.Bridging |
-      ej.diagrams.DiagramConstraints.LineRouting |
-      ej.diagrams.DiagramConstraints.Tooltip,
-    nodes: node,
-    connectors: connector
-  },
-  "#diagram"
-);
-*/
 
 var items = new ej.data.DataManager(data, new ej.data.Query().take(7));
-var i = 1;
+
+ej.diagrams.Diagram.Inject(ej.diagrams.LineRouting);
+
 var diagram = new ej.diagrams.Diagram(
   {
     width: "100%",
     height: "100%",
-    //Uses layout to auto-arrange nodes on the diagram page
+
     layout: {
-      //Sets layout type
       type: "ComplexHierarchicalTree",
       connectionPointOrigin: ej.diagrams.ConnectionPointOrigin.DifferentPoint,
       horizontalSpacing: 50,
       verticalSpacing: 50,
       horizontalAlignment: "Center",
       verticalAlignment: "Center",
-      //margin: { left: 0, right: 0, top: 0, bottom: 0 },
+      margin: { left: 0, right: 0, top: 0, bottom: 0 },
       orientation: "LeftToRight"
     },
 
@@ -404,15 +157,15 @@ var diagram = new ej.diagrams.Diagram(
     },
 
     getNodeDefaults: obj => {
-      obj.width = 40;
-      obj.height = 40;
+      obj.width = 50;
+      obj.height = 50;
       obj.borderWidth = 1;
-      obj.annotations = { content: obj.data.Name };
+      //obj.annotations = { content: obj.data.Name };
       obj.shape = {
         type: "Flow",
         shape: obj.data.shape,
-        cornerRadius: 7
-        //content: obj.data.Name
+        cornerRadius: 7,
+        content: obj.data.Name
       };
 
       if (obj.data.ActiveNode != obj.data.Name) {
@@ -432,8 +185,14 @@ var diagram = new ej.diagrams.Diagram(
     },
 
     getConnectorDefaults: (connector, diagram) => {
+      /*connector.constraints = {
+        constraints:
+          ej.diagrams.ConnectorConstraints.Default |
+          (ej.diagrams.ConnectorConstraints.ReadOnly &
+            ~ej.diagrams.ConnectorConstraints.Select)
+      };*/
       connector.type = "Bezier";
-      connector.cornerRadius = 7;
+      connector.cornerRadius = 10;
       connector.targetDecorator.height = 7;
       connector.targetDecorator.width = 7;
       connector.style = { strokeColor: "#000", strokeWidth: 1 };
@@ -444,11 +203,16 @@ var diagram = new ej.diagrams.Diagram(
 
     constraints:
       ej.diagrams.DiagramConstraints.Default |
+      ej.diagrams.DiagramConstraints.Tooltip |
       ej.diagrams.DiagramConstraints.Bridging |
-      //ej.diagrams.DiagramConstraints.LineRouting,
-      ej.diagrams.DiagramConstraints.Tooltip,
-    shadow: { angle: 40, opacity: 0.8, distance: 10 }
+      ej.diagrams.DiagramConstraints.LineRouting |
+      ej.diagrams.NodeConstraints.Shadow,
+    shadow: { angle: 40, opacity: 0.8, distance: 10 },
+    snapSettings: {
+      constraints: ej.diagrams.SnapConstraints.None
+    }
   },
+
   "#diagram"
 );
 diagram.fitToPage({ mode: "Width" });
